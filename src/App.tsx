008cfb116d5aaced18bc53e822de18370001a4fa
paruripariram <zahovaevvladislav@gmail.com>
2026-03-19 20:07:46 +0300

    Clerk was deleted

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,6 @@
 import './App.css'
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+
 function App(){
-  return  <>
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode="modal"/>
-          <SignUpButton mode="modal"/>
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-    </>
+  return  <> </>
 }
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { ClerkProvider } from "@clerk/react";
-
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
             <App />
-        </ClerkProvider>
     </StrictMode>,
 );
